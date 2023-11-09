37f898fc9f1683e93fd0bd1fcb7e87de1b65f43f
Diego <dvelarde140@gmail.com>
2023-11-09 17:00:44 -0500

    avance en registro

--- proyectStore/src/app/auth/auth-routing.module.ts
+++ proyectStore/src/app/auth/auth-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
     // localhost/4200/auth/
     {
         path: '',
         component: LayoutPageComponent
+    },
+    {
+        path: 'register',
+        component: RegisterPageComponent
     }
 ];
 
