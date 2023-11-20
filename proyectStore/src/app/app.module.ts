af443a6cfeed79585709621a4a96338d5fbedf0e
Diego <dvelarde140@gmail.com>
2023-11-20 17:31:22 -0500

    avanc

--- proyectStore/src/app/app.module.ts
+++ proyectStore/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule, 
     SharedModule
--- proyectStore/src/app/auth/services/heroes.services.ts
+++ proyectStore/src/app/auth/services/heroes.services.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environments } from 'environments/environments';
+import { Hero } from '../interface/hero.interface';
 
 @Injectable({providedIn: 'root'})
 export class ServiceNameService {
+
+    private baseUrl: string = environments.baseUrl;
+
     constructor(private httpClient: HttpClient) { }
     
+    getHeroes():Observable<Hero[]>{
+        return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+    }
+
 }
